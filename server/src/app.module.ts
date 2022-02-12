33a72af9209f6add56843dea116b4da38bd535bf
Vitalii <vitya.canada@gmail.com>
2022-02-13 00:44:05 +0300

    docker and prisma added

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,33 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersController } from './users/users.controller';
-import { User } from './users/user.entity';
-import { UsersService } from './users/users.service';
-import { JwtModule } from '@nestjs/jwt';
-import { Word } from './users/user-words.entity';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService],
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.HOST,
-      port: Number(process.env.PORT),
-      username: process.env.USER,
-      password: process.env.PASSWORD,
-      database: process.env.POSTGRES_DB,
-      entities: [User, Word],
-      retryAttempts: 50,
-      retryDelay: 5000,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    TypeOrmModule.forFeature([User]),
-    JwtModule.register({
-      secret: 'secret',
-      signOptions: { expiresIn: '1d' },
-    }),
-  ],
+  controllers: [],
+  providers: [],
+  imports: [AuthModule, PrismaModule],
 })
 export class AppModule {}
